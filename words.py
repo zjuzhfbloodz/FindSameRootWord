f7534e2bfaea1ea71393b5151fb2b51543f59741
zhfbloodz <2453183055@qq.com>
2020-04-25 09:32:51 +0800

    2020.04.25进一步完善交互界面

--- words.py
+++ words.py
@@ -39,31 +39,41 @@ class WordRoot:
         for i in self.sim_words:
             words_meanings.append((i.split(' ')[0],i.split(' ')[2]))
         title = '\n### {} ：{}\n|单词|释义|\n|:---:|:---:|\n'.format(self.word_root,self.root_meaning)
-        with open(self.path,'a+') as f:
+        with open(self.path,'a+',encoding = 'GB2312') as f:
             f.write(title)
             for i in words_meanings:
                 f.write('|{}|{}|\n'.format(i[0], i[1]))
                 
 def run():
-    with open("D:\Markdown\words.md",'a+') as f:
-        f.write('\n## {}'.format(time.strftime("%Y-%m-%d %H:%M:%S %A", time.localtime())))
+    print("===============================================================")
+    print("    本程序所有选项：'是'请输入'1'，'否'或退出程序请输入'-1'")
+    print("===============================================================")
+    print('\n')
+    write_time = input("请问是否要记录当前的时间：")
+    now_time = time.strftime("%Y-%m-%d %H:%M:%S %A", time.localtime())
+    if write_time == '1':
+        with open("D:\Markdown\words.md",'a+',encoding = 'GB2312') as f:
+            f.write('\n## {}'.format(now_time))
+        print('当前时间是：{}，已写入markdown文件！\n'.format(now_time))
+    else: print("本次笔记未记录时间！！！\n")
     while True:
         word = input('请输入您要查询的单词：')
+        print('\n')
         if word == '-1':
             break
         wr = WordRoot(word)
         if wr.word_root == '':
-            print("抱歉，这个单词目前没有找到词根！")
+            print("抱歉，这个单词目前没有找到词根！\n")
             continue
         print('查询到 {} 的词根是：{}，释义为：{}'.format(word, wr.word_root,wr.root_meaning))
-        print('查询到的同根词有：')
+        print('查询到的同根词共{}个：'.format(len(wr.sim_words)))
         for i in range(len(wr.sim_words)):
             print('{}. {}'.format(i+1,wr.sim_words[i]))
         do = input('请问是否写入笔记： ')
         if do == '1':
             wr.get_mk()
-            print('词根 {} 已写入笔记！！！'.format(wr.word_root))
-        else: print('并未写入笔记！！！')
+            print('词根 {} 已写入笔记！！！\n'.format(wr.word_root))
+        else: print('并未写入笔记！！！\n')
 
 if __name__ == '__main__':
     
